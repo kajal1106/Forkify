ea22b2fd99ed1613fedfab3668a4afc801bfac3b
Kajal Singh <singh.kajal940@gmail.com>
2020-10-04 16:51:12 +0100

    Building the recipe model in progress

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -19,14 +19,5 @@ export default class Recipe{
         }
     }
 
-    calcTime(){
-        // Assuming that we need 15 min for each 3 ingredients
-        const numIng = this.ingredients.length;
-        const periods = Math.ceil(numIng / 3);
-        this.time = periods * 15;
-    }
-
-    calServings(){
-        this.servings = 4;
-    }
+    
 }
