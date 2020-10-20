2b99c2ccd9f98441e93f2024bacd98014eb2f336
Kajal Singh <singh.kajal940@gmail.com>
2020-10-20 19:34:27 +0100

    building recipeView model

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -9,7 +9,17 @@ const formatCount = count => {
     if(count){
         // count=2.5 --> 2 1/2
         // count=0.5 --> 1/2
-        const
+        const [int, dec] = count.toString().split('.').map(el => parseInt(el,10));
+
+        if(!dec) return count;
+
+        if(int === 0){
+            const fr = new Fraction(count);
+            return `${fr.numerator}/${fr.denominator}`;
+        } else {
+            const fr = new Fraction(count - int);
+            return `${int} ${fr.numerator}/${fr.denominator}`;
+        }
     }
     return '?';
 };
