0bc19acaa8e9934e8e7dbccfa636128142d11868
Kajal Singh <singh.kajal940@gmail.com>
2020-10-21 19:55:48 +0100

    Updating Servings

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -88,4 +88,10 @@ export default class Recipe{
         });
         this.ingredients = newIngredients;
     }
+
+    updateServings (type){
+        //Servings
+        const newServings = type === 'dec' ? this.servings - 1 : this.servings + 1;
+        //Ingredients
+    }
 }
