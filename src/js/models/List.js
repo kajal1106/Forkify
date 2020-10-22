6f5b5f88d36757f70c38550f3451674c8b6c7108
Kajal Singh <singh.kajal940@gmail.com>
2020-10-22 17:11:02 +0100

    Updating Shopping model: delete item

--- src/js/models/List.js
+++ src/js/models/List.js
@@ -7,9 +7,16 @@ export default class List{
 
     additem (count, unit, ingredient){
         const item ={
+            id: uniqid,
             count,
             unit,
             ingredient
         }
+    }
+
+    deleteItem (id){
+        const index = this.items.findIndex(el => el.id ===id);
+       // [2,4,8] splice(1,1) --> return 4, original array is [2,8]
+        this.items.splice(index, 1);
     }
 }
